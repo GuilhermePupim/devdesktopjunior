dbf3bae533bb0178c705c4258530f501b46fc9a8
GuilhermePupim <guilherme.hpupim@gmail.com>
2025-04-05 02:52:35 -0300

    DEV - Desktop Junior

---
+++ uCep.pas
@@ -0,0 +1,40 @@
+unit uCep;
+
+interface
+
+type
+TCep = class
+
+private
+FCep : String;
+FLogradouro : String;
+FComplemento : String;
+FBairro : String;
+FLocalidade : String;
+FUf : String;
+FIbge : String;
+FGia : String;
+FDdd : String;
+FSiafi : String;
+FJson: String;
+
+
+public
+Cep : String;
+Logradouro : String;
+Complemento : String;
+Bairro : String;
+Localidade : String;
+Uf : String;
+Ibge : String;
+Gia : String;
+Ddd : String;
+Siafi : String;
+Json: String;
+
+end;
+
+implementation
+
+
+end.
